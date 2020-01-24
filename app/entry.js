22a3a542ccf9e139b9ba47655ea2921908447713
Shikiji <56910224+shikij1@users.noreply.github.com>
2020-01-24 22:57:46 +0900

    practice jquery

---
+++ app/entry.js
@@ -0,0 +1,15 @@
+"use strict";
+import $ from "jquery";
+const block = $("#block");
+const scalingButton = $("#scaling-button");
+const movingButton = $("#moving-button");
+
+scalingButton.click(() => {
+  block.animate({ width: "200pt", height: "200pt" }, 2000);
+  block.animate({ width: "100pt", height: "100pt" }, 2000);
+});
+
+movingButton.click(() => {
+  block.animate({ marginLeft: "500px" }, 2000);
+  block.animate({ marginLeft: "20px" }, 2000);
+});
